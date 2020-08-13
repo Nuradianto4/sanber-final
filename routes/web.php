b5df1eb397e638097957339990b34904be3a309b
Nuradianto4 <adhisopo44@gmail.com>
2020-08-13 16:13:31 +0700

    cek route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return view('layouts.user.master');
-});
+
